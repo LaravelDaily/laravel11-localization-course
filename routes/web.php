ca5b20d3186c099ce051dea4c31cb4c67d2b36be
modestasv <info@modestasv.lt>
2024-08-21 16:46:16 +0300

    Example of UI language switcher with database

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,27 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ChangeLanguageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\SetLocale;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('lang/{locale}', ChangeLanguageController::class)->name('change-locale');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(SetLocale::class)
+    ->group(function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__ . '/auth.php';
+        Route::middleware('auth')->group(function () {
+            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+            Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        });
+
+        require __DIR__ . '/auth.php';
+    });
