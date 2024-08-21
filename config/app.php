b84fac90b29d8238805b7de81b3eb0062a1e852d
modestasv <info@modestasv.lt>
2024-08-21 16:49:39 +0300

    Translated content without packages

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -79,6 +79,11 @@ return [
     */
 
     'locale' => env('APP_LOCALE', 'en'),
+
+    'supportedLocales' => [
+        'en',
+        'es',
+    ],
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
